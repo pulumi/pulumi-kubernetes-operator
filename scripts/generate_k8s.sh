8f046fe6acbd3be57fb10a7bda212c6b2d2df65f
Mike Metral <1112768+metral@users.noreply.github.com>
2020-07-16 23:33:10 +0000

    build: replace operator-sdk generate k8s with controller-gen
    `operator-sdk generate <subcommand>` is being removed in a [breaking change](https://github.com/operator-framework/operator-sdk/blob/master/changelog/fragments/rm-legacy-go.yaml).
    
    Moving to controller-gen instead per [kubebuilder docs](https://book.kubebuilder.io/reference/generating-crd.html#under-the-hood), and for parity with controller-gen usage in `./scripts/generate_crds.sh`

--- scripts/generate_k8s.sh
+++ scripts/generate_k8s.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 
+cwd=$(dirname "$0")
+apis_dir="$cwd/../pkg/apis"
+
 echo "Updating the CRD k8s deepcopy code..."
-operator-sdk generate k8s
+go run sigs.k8s.io/controller-tools/cmd/controller-gen object paths="$apis_dir/..."
