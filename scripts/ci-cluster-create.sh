fca0abf1d6d916dcc39b57662e781c37c20c9f47
Mike Metral <1112768+metral@users.noreply.github.com>
2020-07-27 20:26:38 +0000

    build: add GHA CI integration tests and ephemeral GKE cluster setup

---
+++ scripts/ci-cluster-create.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o nounset -o errexit -o pipefail
+
+echo Creating ephemeral Kubernetes cluster for CI testing...
+
+pushd test/ci-cluster
+yarn install --json --verbose >out
+tail -10 out
+pulumi stack init "${STACK}"
+pulumi up --skip-preview --yes
+
+mkdir -p "$HOME/.kube/"
+pulumi stack output kubeconfig >~/.kube/config
+
+popd
---
+++ scripts/ci-cluster-destroy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o nounset -o errexit -o pipefail
+
+echo Deleting ephemeral Kubernetes cluster...
+
+pushd test/ci-cluster
+pulumi stack select "${STACK}" && \
+  pulumi destroy --skip-preview --yes && \
+  pulumi stack rm --yes
+
+popd
