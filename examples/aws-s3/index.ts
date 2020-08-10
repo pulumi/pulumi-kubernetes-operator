22390c4e6385963b221a7c0bed9507f797db9184
Mike Metral <1112768+metral@users.noreply.github.com>
2020-08-10 19:29:04 +0000

    examples: add aws-s3 example project

---
+++ examples/aws-s3/index.ts
@@ -0,0 +1,64 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as k8s from "@pulumi/kubernetes";
+import * as kx from "@pulumi/kubernetesx";
+import * as operator from "./operator";
+
+// #############################################################################
+// Deploy the Pulumi Kubernetes Operator
+
+// By default, uses $HOME/.kube/config when no kubeconfig is set.
+const provider = new k8s.Provider("k8s"); 
+
+// Create the Pulumi Kubernetes Operator.
+// Uses a custom ComponentResource class based on Typescript code in https://git.io/JJ6yj
+const name = "pulumi-k8s-operator"
+const pulumiOperator = new operator.PulumiKubernetesOperator(name, {
+    namespace: "default",
+    provider,
+});
+
+// #############################################################################
+// Deploy AWS S3 Buckets
+
+// Get the Pulumi API token and AWS creds.
+const config = new pulumi.Config();
+
+const pulumiAccessToken = config.requireSecret("pulumiAccessToken")
+
+const awsAccessKeyId = config.require("awsAccessKeyId")
+const awsSecretAccessKey = config.requireSecret("awsSecretAccessKey")
+const awsSessionToken = config.requireSecret("awsSessionToken")
+
+const stackName = config.require("stackName")
+const stackProjectRepo = config.require("stackProjectRepo")
+const stackCommit = config.require("stackCommit")
+
+// Create the creds as Kubernetes Secrets.
+const accessToken = new kx.Secret("accesstoken", {
+    stringData: { accessToken: pulumiAccessToken},
+});
+const awsCreds = new kx.Secret("aws-creds", {
+    stringData: { 
+        "AWS_ACCESS_KEY_ID": awsAccessKeyId,
+        "AWS_SECRET_ACCESS_KEY": awsSecretAccessKey,
+        "AWS_SESSION_TOKEN": awsSessionToken,
+    },
+});
+
+// Create an AWS S3 Pulumi Stack in Kubernetes.
+const mystack = new k8s.apiextensions.CustomResource("my-stack", {
+    apiVersion: 'pulumi.com/v1alpha1',
+    kind: 'Stack',
+    spec: {
+        stack: stackName,
+        projectRepo: stackProjectRepo,
+        commit: stackCommit,
+        accessTokenSecret: accessToken.metadata.name,
+        config: {
+            "aws:region": "us-west-2",
+        },
+        envSecrets: [awsCreds.metadata.name],
+        initOnCreate: true,
+        destroyOnFinalize: true,
+    },
+}, {dependsOn: pulumiOperator.deployment});
