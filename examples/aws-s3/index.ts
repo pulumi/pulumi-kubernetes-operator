cf3a40887178f40c5752115040d9212fe319acce
Vivek Lakshmanan <vivek@pulumi.com>
2021-04-29 11:33:48 -0700

    Copyright headers

--- examples/aws-s3/index.ts
+++ examples/aws-s3/index.ts
@@ -1,3 +1,5 @@
+// Copyright 2021, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 import * as kx from "@pulumi/kubernetesx";
