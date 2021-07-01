3973f30fa911ba341af72e4fddc9f3b00b38367e
Vivek Lakshmanan <vivek@pulumi.com>
2021-07-01 15:49:43 -0700

    Install controller-gen separately as well

--- scripts/generate_crds.sh
+++ scripts/generate_crds.sh
@@ -7,9 +7,11 @@ deploy_dir="$cwd/../deploy/crds"
 
 echo "Generating CRD API types..."
 
-go run sigs.k8s.io/controller-tools/cmd/controller-gen crd paths="$apis_dir/..." crd:crdVersions=v1 output:crd:dir="$deploy_dir"
+controller-gen crd paths="$apis_dir/..." crd:crdVersions=v1 output:crd:dir="$deploy_dir"
 
 # Manually overwrite until issue is resolved in controller-tools:
 # https://git.io/JJsjs
+# Requires gnu-sed. On Macs you might have to do the following: 
+# export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 sed -i "s#conditions: null#conditions: []#g" "$deploy_dir/pulumi.com_stacks.yaml"
 sed -i "s#storedVersions: null#storedVersions: []#g" "$deploy_dir/pulumi.com_stacks.yaml"
