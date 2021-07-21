0580bdb1896b3489fb6df29fa3fcb3006584de17
Levi Blackstone <levi@pulumi.com>
2021-07-21 10:45:24 -0600

    Only poll on branches without a specified commit

--- pkg/controller/stack/stack_controller.go
+++ pkg/controller/stack/stack_controller.go
@@ -209,14 +209,19 @@ func (r *ReconcileStack) Reconcile(request reconcile.Request) (reconcile.Result,
 		}
 	}
 
-	reqLogger.Info("Checking current HEAD commit hash", "Current commit", currentCommit)
-	if instance.Status.LastUpdate.LastSuccessfulCommit == currentCommit {
-		reqLogger.Info("Commit hash unchanged. Will poll again in 60 seconds.")
-		// Reconcile every 60 seconds to support git branch tracking.
-		return reconcile.Result{RequeueAfter: 60 * time.Second}, nil
+	// If a branch is specified without a commit, then track changes to the branch.
+	trackBranch := len(sess.stack.Branch) > 0 && len(sess.stack.Commit) == 0
+
+	if trackBranch {
+		reqLogger.Info("Checking current HEAD commit hash", "Current commit", currentCommit)
+		if instance.Status.LastUpdate.LastSuccessfulCommit == currentCommit {
+			reqLogger.Info("Commit hash unchanged. Will poll again in 60 seconds.")
+			// Reconcile every 60 seconds to support git branch tracking.
+			return reconcile.Result{RequeueAfter: 60 * time.Second}, nil
+		}
+		reqLogger.Info("New commit hash found", "Current commit", currentCommit,
+			"Last commit", instance.Status.LastUpdate.LastSuccessfulCommit)
 	}
-	reqLogger.Info("New commit hash found", "Current commit", currentCommit,
-		"Last commit", instance.Status.LastUpdate.LastSuccessfulCommit)
 
 	// Step 3. If a stack refresh is requested, run it now.
 	if sess.stack.Refresh {
@@ -304,8 +309,12 @@ func (r *ReconcileStack) Reconcile(request reconcile.Request) (reconcile.Result,
 	}
 	reqLogger.Info("Successfully updated status for Stack", "Stack.Name", stack.Stack)
 
-	// Reconcile every 60 seconds to support git branch tracking.
-	return reconcile.Result{RequeueAfter: 60 * time.Second}, nil
+	if trackBranch {
+		// Reconcile every 60 seconds to check for new commits to the branch.
+		return reconcile.Result{RequeueAfter: 60 * time.Second}, nil
+	}
+
+	return reconcile.Result{}, nil
 }
 
 func (sess *reconcileStackSession) finalize(stack *pulumiv1alpha1.Stack) error {
