253072ad6ab775419f37de925ec2d9eeb808b3ea
Vivek Lakshmanan <vivek@pulumi.com>
2021-08-18 06:27:03 -0700

    Add test data for validating stack outputs (#176)

--- test/testdata/empty-stack/index.ts
+++ test/testdata/empty-stack/index.ts
@@ -1,2 +1,8 @@
+import * as pulumi from "@pulumi/pulumi";
+
 console.log("Empty stack!");
 
+export const notSoSecret = pulumi.output("safe")
+export const secretVal = pulumi.secret({
+    "val" : "very secret"
+});
