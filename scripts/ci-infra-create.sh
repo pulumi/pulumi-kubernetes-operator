0a0f632a2ae3d0cb15244856d9ab2b8314915f7e
Michael Bridgen <mbridgen@pulumi.com>
2022-08-31 17:32:47 +0100

    Remove cluster setup from github workflow
    Signed-off-by: Michael Bridgen <mbridgen@pulumi.com>

--- scripts/ci-infra-create.sh
+++ scripts/ci-infra-create.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 set -o nounset -o errexit -o pipefail
 
-echo Creating ephemeral Kubernetes cluster for CI testing...
-
-pushd test/ci-cluster
-yarn install --json --verbose >out
-tail -10 out
-pulumi stack init "${STACK}"
-pulumi up --skip-preview --yes
-
-mkdir -p "$HOME/.kube/"
-pulumi stack output kubeconfig --show-secrets >~/.kube/config
-popd
-
 echo Creating S3 backend and KMS key
 
 pushd test/s3backend
