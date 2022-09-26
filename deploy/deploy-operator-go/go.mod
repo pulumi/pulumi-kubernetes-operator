f33b740c1f2dd36b26775db0363e38a400361c66
Michael Bridgen <mbridgen@pulumi.com>
2022-09-26 15:27:20 +0100

    Parameterise the namespace(s) and versions
    This commit makes it possible to install the operator in several
    namespaces, by supplying multiple values in the config item
    `namespaces` when running one of the Pulumi programs under deploy/.
    
    (This also makes the deployed operator log level `error` in the Go and
    .NET programs, to bring them in line with the other programs and
    canonical YAML configuration.)
    
    The CRD and operator tags are now items of config, for each of the
    programs under deploy/, with defaults. An upgrade is then a case of
    e.g.,
    
        pulumi config set operator-version v1.10.2
        pulumi up
    
    The defaults are consts at the top of the file, and can be updated more
    easily.
    
    Signed-off-by: Michael Bridgen <mbridgen@pulumi.com>

--- deploy/deploy-operator-go/go.mod
+++ deploy/deploy-operator-go/go.mod
@@ -3,6 +3,6 @@ module deploy-operator-go
 go 1.14
 
 require (
-	github.com/pulumi/pulumi-kubernetes/sdk/v3 v3.8.3
-	github.com/pulumi/pulumi/sdk/v3 v3.17.0
+	github.com/pulumi/pulumi-kubernetes/sdk/v3 v3.22.1
+	github.com/pulumi/pulumi/sdk/v3 v3.43.1
 )
--- deploy/deploy-operator-py/__main__.py
+++ deploy/deploy-operator-py/__main__.py
@@ -2,6 +2,15 @@ import pulumi
 from pulumi.resource import ResourceOptions
 import pulumi_kubernetes as kubernetes
 
+DEFAULT_CRD_VERSION = "v1.10.1"
+DEFAULT_OPERATOR_VERSION = "v1.10.1"
+
+conf = pulumi.Config()
+namespace = conf.get("namespace", "default")
+namespaces = conf.get_object("namespaces", [namespace])
+crdVersion = conf.get("crd-version", DEFAULT_CRD_VERSION)
+operatorVersion = conf.get("operator-version", DEFAULT_OPERATOR_VERSION)
+
 # Work around https://github.com/pulumi/pulumi-kubernetes/issues/1481
 def delete_status():
     def f(o):
@@ -9,170 +18,189 @@ def delete_status():
             del o["status"]
     return f
 
-crds = kubernetes.yaml.ConfigFile("crds",
-    file="https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/v1.10.1/deploy/crds/pulumi.com_stacks.yaml",
+stack_crd = kubernetes.yaml.ConfigFile(
+    "stackcrd",
+    file="https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/%s/deploy/crds/pulumi.com_stacks.yaml" % crdVersion,
     transformations=[delete_status()])
+program_crd = kubernetes.yaml.ConfigFile(
+    "programcrd",
+    file="https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/%s/deploy/crds/pulumi.com_programs.yaml" % crdVersion,
+    transformations=[delete_status()])
+deployment_opts = ResourceOptions(depends_on=[stack_crd, program_crd])
 
-operator_service_account = kubernetes.core.v1.ServiceAccount("operator-service-account")
-operator_role = kubernetes.rbac.v1.Role("operator-role",
-    rules=[
-        {
-            "api_groups": [""],
-            "resources": [
-                "pods",
-                "services",
-                "services/finalizers",
-                "endpoints",
-                "persistentvolumeclaims",
-                "events",
-                "configmaps",
-                "secrets",
-            ],
-            "verbs": [
-                "create",
-                "delete",
-                "get",
-                "list",
-                "patch",
-                "update",
-                "watch",
-            ],
-        },
-        {
-            "api_groups": ["apps"],
-            "resources": [
-                "deployments",
-                "daemonsets",
-                "replicasets",
-                "statefulsets",
-            ],
-            "verbs": [
-                "create",
-                "delete",
-                "get",
-                "list",
-                "patch",
-                "update",
-                "watch",
-            ],
-        },
-        {
-            "api_groups": ["monitoring.coreos.com"],
-            "resources": ["servicemonitors"],
-            "verbs": [
-                "create",
-                "get",
-            ],
-        },
-        {
-            "api_groups": ["apps"],
-            "resource_names": ["pulumi-kubernetes-operator"],
-            "resources": ["deployments/finalizers"],
-            "verbs": ["update"],
-        },
-        {
-            "api_groups": [""],
-            "resources": ["pods"],
-            "verbs": ["get"],
-        },
-        {
-            "api_groups": ["apps"],
-            "resources": [
-                "replicasets",
-                "deployments",
-            ],
-            "verbs": ["get"],
-        },
-        {
-            "api_groups": ["pulumi.com"],
-            "resources": ["*"],
-            "verbs": [
-                "create",
-                "delete",
-                "get",
-                "list",
-                "patch",
-                "update",
-                "watch",
-            ],
-        },
-        {
-            "api_groups": ["coordination.k8s.io"],
-            "resources": ["leases"],
-            "verbs": [
-                "create",
-                "get",
-                "list",
-                "update",
-            ],
-        }, 
-    ])
-operator_role_binding = kubernetes.rbac.v1.RoleBinding("operator-role-binding",
-    subjects=[{
-        "kind": "ServiceAccount",
-        "name": operator_service_account.metadata.name,
-    }],
-    role_ref={
-        "kind": "Role",
-        "name": operator_role.metadata.name,
-        "api_group": "rbac.authorization.k8s.io",
-    })
-operator_deployment = kubernetes.apps.v1.Deployment("pulumi-kubernetes-operator",
-    spec={
-        "replicas": 1,
-        "selector": {
-            "match_labels": {
-                "name": "pulumi-kubernetes-operator",
-            },
-        },
-        "template": {
-            "metadata": {
-                "labels": {
-                    "name": "pulumi-kubernetes-operator",
-                },
-            },
-            "spec": {
-                "service_account_name": operator_service_account.metadata.name,
-                "containers": [{
-                    "name": "pulumi-kubernetes-operator",
-                    "image": "pulumi/pulumi-kubernetes-operator:v1.10.1",
-                    "command": ["pulumi-kubernetes-operator"],
-                    "args": ["--zap-level=error", "--zap-time-encoding=iso8601"],
-                    "image_pull_policy": "Always",
-                    "env": [
-                        {
-                            "name": "WATCH_NAMESPACE",
-                            "value_from": {
-                                "field_ref": {
-                                    "field_path": "metadata.namespace",
-                                },
-                            },
-                        },
-                        {
-                            "name": "POD_NAME",
-                            "value_from": {
-                                "field_ref": {
-                                    "field_path": "metadata.name",
-                                },
-                            },
-                        },
-                        {
-                            "name": "OPERATOR_NAME",
-                            "value": "pulumi-kubernetes-operator",
-                        },
-                        {
-                            "name": "GRACEFUL_SHUTDOWN_TIMEOUT_DURATION",
-                            "value": "5m",
-                        },
-                        {
-                            "name": "MAX_CONCURRENT_RECONCILES",
-                            "value": "10",
-                        },
-                    ],
-                }],
-            },
-            # Should be same or larger than GRACEFUL_SHUTDOWN_TIMEOUT_DURATION
-            "terminationGracePeriodSeconds": 300,
-        },
-    },
-    opts=ResourceOptions(depends_on=crds))
+for ns in namespaces:
+    
+    operator_service_account = kubernetes.core.v1.ServiceAccount("operator-service-account-%s" % (ns),
+                                                                 metadata={"namespace": ns})
+    operator_role = kubernetes.rbac.v1.Role("operator-role-%s" % (ns),
+                                            metadata={
+                                                "namespace": ns,
+                                            },
+                                            rules=[
+                                                {
+                                                    "api_groups": [""],
+                                                    "resources": [
+                                                        "pods",
+                                                        "services",
+                                                        "services/finalizers",
+                                                        "endpoints",
+                                                        "persistentvolumeclaims",
+                                                        "events",
+                                                        "configmaps",
+                                                        "secrets",
+                                                    ],
+                                                    "verbs": [
+                                                        "create",
+                                                        "delete",
+                                                        "get",
+                                                        "list",
+                                                        "patch",
+                                                        "update",
+                                                        "watch",
+                                                    ],
+                                                },
+                                                {
+                                                    "api_groups": ["apps"],
+                                                    "resources": [
+                                                        "deployments",
+                                                        "daemonsets",
+                                                        "replicasets",
+                                                        "statefulsets",
+                                                    ],
+                                                    "verbs": [
+                                                        "create",
+                                                        "delete",
+                                                        "get",
+                                                        "list",
+                                                        "patch",
+                                                        "update",
+                                                        "watch",
+                                                    ],
+                                                },
+                                                {
+                                                    "api_groups": ["monitoring.coreos.com"],
+                                                    "resources": ["servicemonitors"],
+                                                    "verbs": [
+                                                        "create",
+                                                        "get",
+                                                    ],
+                                                },
+                                                {
+                                                    "api_groups": ["apps"],
+                                                    "resource_names": ["pulumi-kubernetes-operator"],
+                                                    "resources": ["deployments/finalizers"],
+                                                    "verbs": ["update"],
+                                                },
+                                                {
+                                                    "api_groups": [""],
+                                                    "resources": ["pods"],
+                                                    "verbs": ["get"],
+                                                },
+                                                {
+                                                    "api_groups": ["apps"],
+                                                    "resources": [
+                                                        "replicasets",
+                                                        "deployments",
+                                                    ],
+                                                    "verbs": ["get"],
+                                                },
+                                                {
+                                                    "api_groups": ["pulumi.com"],
+                                                    "resources": ["*"],
+                                                    "verbs": [
+                                                        "create",
+                                                        "delete",
+                                                        "get",
+                                                        "list",
+                                                        "patch",
+                                                        "update",
+                                                        "watch",
+                                                    ],
+                                                },
+                                                {
+                                                    "api_groups": ["coordination.k8s.io"],
+                                                    "resources": ["leases"],
+                                                    "verbs": [
+                                                        "create",
+                                                        "get",
+                                                        "list",
+                                                        "update",
+                                                    ],
+                                                }, 
+                                            ])
+
+    operator_role_binding = kubernetes.rbac.v1.RoleBinding("operator-role-binding-%s" % (ns),
+                                                       metadata={
+                                                           "namespace": ns,
+                                                       },
+                                                       subjects=[{
+                                                           "kind": "ServiceAccount",
+                                                           "name": operator_service_account.metadata.name,
+                                                       }],
+                                                       role_ref={
+                                                           "kind": "Role",
+                                                           "name": operator_role.metadata.name,
+                                                           "api_group": "rbac.authorization.k8s.io",
+                                                       })
+
+    operator_deployment = kubernetes.apps.v1.Deployment("pulumi-kubernetes-operator-%s" % (ns),
+                                                    metadata={
+                                                        "namespace": ns,
+                                                    },
+                                                    spec={
+                                                        "replicas": 1,
+                                                        "selector": {
+                                                            "match_labels": {
+                                                                "name": "pulumi-kubernetes-operator",
+                                                            },
+                                                        },
+                                                        "template": {
+                                                            "metadata": {
+                                                                "labels": {
+                                                                    "name": "pulumi-kubernetes-operator",
+                                                                },
+                                                            },
+                                                            "spec": {
+                                                                "service_account_name": operator_service_account.metadata.name,
+                                                                "containers": [{
+                                                                    "name": "pulumi-kubernetes-operator",
+                                                                    "image": "pulumi/pulumi-kubernetes-operator:%s" % operatorVersion,
+                                                                    "command": ["pulumi-kubernetes-operator"],
+                                                                    "args": ["--zap-level=error", "--zap-time-encoding=iso8601"],
+                                                                    "image_pull_policy": "Always",
+                                                                    "env": [
+                                                                        {
+                                                                            "name": "WATCH_NAMESPACE",
+                                                                            "value_from": {
+                                                                                "field_ref": {
+                                                                                    "field_path": "metadata.namespace",
+                                                                                },
+                                                                            },
+                                                                        },
+                                                                        {
+                                                                            "name": "POD_NAME",
+                                                                            "value_from": {
+                                                                                "field_ref": {
+                                                                                    "field_path": "metadata.name",
+                                                                                },
+                                                                            },
+                                                                        },
+                                                                        {
+                                                                            "name": "OPERATOR_NAME",
+                                                                            "value": "pulumi-kubernetes-operator",
+                                                                        },
+                                                                        {
+                                                                            "name": "GRACEFUL_SHUTDOWN_TIMEOUT_DURATION",
+                                                                            "value": "5m",
+                                                                        },
+                                                                        {
+                                                                            "name": "MAX_CONCURRENT_RECONCILES",
+                                                                            "value": "10",
+                                                                        },
+                                                                    ],
+                                                                }],
+                                                            },
+                                                            # Should be same or larger than GRACEFUL_SHUTDOWN_TIMEOUT_DURATION
+                                                            "terminationGracePeriodSeconds": 300,
+                                                        },
+                                                    }, opts=deployment_opts)
