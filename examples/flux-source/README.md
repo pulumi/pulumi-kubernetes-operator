b7f30ccfe064cfe5ce5af869d05976baeef2a2c6
Michael Bridgen <mbridgen@pulumi.com>
2022-10-24 19:44:07 +0100

    Example of deploying the operator with Flux source controller (#339)
    * Document Flux+Pulumi example
    
    This has two Pulumi projects:
    
     - the top directory (examples/flux-source) is for deploying the Pulumi
       Kubernetes operator along with the Flux source controller
    
     - the subdirectory app/ has an example a a Stack object refering to a
       Flux source.
    
    * Include webhook receiver in app stack
    
     - include Flux's notification-controller when installing Flux
     - create a webhook receiver object
    
    This parameterises the git URL used for the GitRepository source, so you
    must supply your own. (People won't be able to install webhooks in my
    repo!)
    
    * Supply an example of deploying ngrok
    
    You can use ngrok to tunnel webhooks through to a local or otherwise
    private cluster. This gives an example of deploying it which is enough
    to make the webhook in ../app work.
    
    * Mention Flux example in changelog
    
    Signed-off-by: Michael Bridgen <mbridgen@pulumi.com>

---
+++ examples/flux-source/README.md
@@ -0,0 +1,136 @@
+# Example of using the Pulumi Kubernetes operator with Flux
+
+The Pulumi operator can use [Flux sources][] for getting Pulumi programs. This example shows how to
+set the operator, and Flux source controller, up; and, how to create a `GitRepository` (a Flux
+source) and a `Stack` object that points to it.
+
+## Running the installation program
+
+The project in this directory will:
+
+ - install the Flux source CRDs and controller
+ - install the Pulumi operator and its CRD(s)
+ - give the Pulumi operator permissions to see Flux source objects
+
+You'll need to install the program dependencies:
+
+```console
+flux-source$ npm install
+```
+
+Now you can run the stack with the Pulumi command-line:
+
+```console
+flux-source$ pulumi up
+```
+
+## Running the example app Stack
+
+You will need to install the dependencies for the program:
+
+```console
+app$ npm install
+```
+
+For the project in `app/`, you'll need a Pulumi access token. You can create one in the [Pulumi
+service][] under "Access tokens". Assign it to the environment variable
+`PULUMI_ACCESS_TOKEN`.
+
+You will also need to supply a git repository URL. You can use my example:
+`https://github.com/squaremo/pko-dev`, but you won't be able to install webhooks there, so it might
+pay to fork that or create your own. (If you do create your own, you will likely want to change the
+`dir` in the Stack spec, which gives the subdirectory of the repo where your Pulumi code is).
+
+To run the program:
+
+```console
+app$ pulumi config set git-url https://github.com/squaremo/pko-dev
+app$ pulumi config set stack-name $(pulumi whoami)/app/dev
+app$ pulumi up
+```
+
+This creates a `GitRepository` object in the cluster, which you can examine:
+
+```console
+app$ kubectl get gitrepository
+```
+
+and a `Stack` object, pointing at the `GitRepository` object, which will run the program and create
+a deployment. You watch the stack object until it has completed:
+
+```console
+app$ kubectl get stack -w
+```
+
+You should see the state become `succeeded` in a minute or so. The result of the stack is a
+deployment:
+
+```console
+app$ kubectl get deployment
+```
+
+You can also look in the [Pulumi service][] to see what it creates.
+
+## Webhooks
+
+The program in `app/` also creates a webhook receiver, so you can get notifications from GitHub when
+there are new commits pushed. You will need to expose the webhook service to the internet for this
+to work: either use the guide in
+https://fluxcd.io/flux/guides/webhook-receivers/#expose-the-webhook-receiver, or use something like
+ngrok to tunnel the webhook into a private cluster. If you use the Flux guide, be aware that the
+webhook receiver here is in `default` namespace, rather than `flux-system`. There's a working
+example for ngrok in `ngrok/`.
+
+### Using ngrok to tunnel webhooks
+
+The directory `ngrok/` has a program that will run ngrok in such a way as to make the webhooks
+work. It doesn't need any configuration; just:
+
+```console
+ngrok$ pulumi up
+```
+
+To see the public hostname that it's set up, access the ngrok console:
+
+```console
+ngrok$ NGROK_SERVICE=$(pulumi stack output service)
+ngrok$ kubectl port-forward service/$NGROK_SERVICE 4040:80
+```
+
+The ngrok console will then be reachable on http://localhost:4040/.
+
+Bear in mind that the tunnel hostname will change every time this restarts, so it's only really
+useful for (shortlived) demos.
+
+## Installing development versions
+
+You can use the install program to run a locally-built image, with the following steps.
+
+ * install the CRD over the top of that installed by the stack
+
+```console
+pulumi-kubernetes-operator$ make install-crds
+```
+
+ * build a development version of the image:
+
+```console
+pulumi-kubernetes-operator$ make build-image IMAGE_NAME=pulumi/pulumi-kubernetes-operator
+```
+
+(`IMAGE_NAME` means it won't name the image after `$(whoami)`)
+
+> If you're using `kind` to run a local Kubernetes cluster, you will need to side-load the image:
+>
+> ```console
+> kind load docker-image pulumi/pulumi-kubernetes-operator:$(git rev-parse --short HEAD)
+> ```
+
+ * use that image's tag as the `operator-version` configuration item when running the stack:
+
+```console
+pulumi-kubernetes-operator/examples/flux-source$ pulumi up -c operator-version=$(git rev-parse --short HEAD)
+```
+
+[Flux source]: https://fluxcd.io/flux/components/source/api/
+[Pulumi service]: https://app.pulumi.com/
---
+++ examples/flux-source/index.ts
@@ -0,0 +1,251 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as kubernetes from "@pulumi/kubernetes";
+import { ConfigGroup } from "@pulumi/kubernetes/yaml";
+import * as flux from "@worawat/flux";
+
+const config = new pulumi.Config();
+const deployNamespace = config.get("namespace") || 'default';
+const deployNamespaceList = config.getObject<string[]>("namespaces") || [deployNamespace];
+const operatorVersion = config.get("operator-version") || "v1.10.0";
+const crdVersion = config.get("crd-version") || "v1.10.0";
+
+// -- Flux installation
+
+const fluxManifests = flux.getFluxInstallOutput({
+    targetPath: "local",
+    components: ['source-controller', 'notification-controller'],
+    networkPolicy: true,
+});
+
+// this effectively installs the Flux controllers
+const fluxGroup = fluxManifests.content.apply(c => new ConfigGroup("flux-install", { yaml: c }));
+
+// --- Pulumi operator
+
+const stackcrd = new kubernetes.yaml.ConfigFile('stack-crd', {
+    file: `https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/${crdVersion}/deploy/crds/pulumi.com_stacks.yaml`,
+});
+const programcrd = new kubernetes.yaml.ConfigFile('program-crd', {
+    file: `https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/${crdVersion}/deploy/crds/pulumi.com_programs.yaml`,
+});
+
+const deploymentOptions = { dependsOn: [stackcrd, programcrd, fluxGroup] };
+
+for (let ns of deployNamespaceList) {
+    const operatorServiceAccount = new kubernetes.core.v1.ServiceAccount(`operator-service-account-${ns}`, {
+        metadata: {
+            "namespace": ns,
+        },
+    });
+
+    const operatorRole = new kubernetes.rbac.v1.Role(`operator-role-${ns}`, {
+        metadata: {
+            "namespace": ns,
+        },
+        rules: [
+            {
+                apiGroups: [""],
+                resources: [
+                    "pods",
+                    "services",
+                    "services/finalizers",
+                    "endpoints",
+                    "persistentvolumeclaims",
+                    "events",
+                    "configmaps",
+                    "secrets",
+                ],
+                verbs: [
+                    "create",
+                    "delete",
+                    "get",
+                    "list",
+                    "patch",
+                    "update",
+                    "watch",
+                ],
+            },
+            {
+                apiGroups: ["apps"],
+                resources: [
+                    "deployments",
+                    "daemonsets",
+                    "replicasets",
+                    "statefulsets",
+                ],
+                verbs: [
+                    "create",
+                    "delete",
+                    "get",
+                    "list",
+                    "patch",
+                    "update",
+                    "watch",
+                ],
+            },
+            {
+                apiGroups: ["monitoring.coreos.com"],
+                resources: ["servicemonitors"],
+                verbs: [
+                    "create",
+                    "get",
+                ],
+            },
+            {
+                apiGroups: ["apps"],
+                resourceNames: ["pulumi-kubernetes-operator"],
+                resources: ["deployments/finalizers"],
+                verbs: ["update"],
+            },
+            {
+                apiGroups: [""],
+                resources: ["pods"],
+                verbs: ["get"],
+            },
+            {
+                apiGroups: ["apps"],
+                resources: [
+                    "replicasets",
+                    "deployments",
+                ],
+                verbs: ["get"],
+            },
+            {
+                apiGroups: ["pulumi.com"],
+                resources: ["*"],
+                verbs: [
+                    "create",
+                    "delete",
+                    "get",
+                    "list",
+                    "patch",
+                    "update",
+                    "watch",
+                ],
+            },
+
+            {
+                apiGroups: ["coordination.k8s.io"],
+                resources: ["leases"],
+                verbs: [
+                    "create",
+                    "get",
+                    "list",
+                    "update",
+                ],
+            },
+        ],
+    });
+
+    const fluxAccessRole = new kubernetes.rbac.v1.Role(`flux-role-${ns}`, {
+        metadata: {
+            "namespace": ns,
+        },
+        rules: [
+            // this is specifically to give access to Flux sources
+            {
+                apiGroups: ["source.toolkit.fluxcd.io"],
+                resources: ["*"],
+                verbs: [
+                    "get",
+                    "list",
+                    "watch",
+                ],
+            },
+        ],
+    });
+    
+    const operatorRoleBinding = new kubernetes.rbac.v1.RoleBinding(`operator-role-binding-${ns}`, {
+        metadata: {
+            "namespace": ns,
+        },
+        subjects: [{
+            kind: "ServiceAccount",
+            name: operatorServiceAccount.metadata.name,
+        }],
+        roleRef: {
+            kind: "Role",
+            name: operatorRole.metadata.name,
+            apiGroup: "rbac.authorization.k8s.io",
+        },
+    });
+
+    const fluxRoleBinding = new kubernetes.rbac.v1.RoleBinding(`flux-role-binding-${ns}`, {
+        metadata: {
+            "namespace": ns,
+        },
+        subjects: [{
+            kind: "ServiceAccount",
+            name: operatorServiceAccount.metadata.name,
+        }],
+        roleRef: {
+            kind: "Role",
+            name: fluxAccessRole.metadata.name,
+            apiGroup: "rbac.authorization.k8s.io",
+        },
+    });
+
+    const operatorDeployment = new kubernetes.apps.v1.Deployment(`pulumi-kubernetes-operator-${ns}`, {
+        metadata: {
+            "namespace": ns,
+        },
+        spec: {
+            replicas: 1,
+            selector: {
+                matchLabels: {
+                    name: "pulumi-kubernetes-operator",
+                },
+            },
+            template: {
+                metadata: {
+                    labels: {
+                        name: "pulumi-kubernetes-operator",
+                    },
+                },
+                spec: {
+                    serviceAccountName: operatorServiceAccount.metadata.name,
+                    containers: [{
+                        name: "pulumi-kubernetes-operator",
+                        image: `pulumi/pulumi-kubernetes-operator:${operatorVersion}`,
+                        imagePullPolicy: "IfNotPresent",
+                        args: ["--zap-level=error", "--zap-time-encoding=iso8601"],
+                        env: [
+                            {
+                                name: "WATCH_NAMESPACE",
+                                valueFrom: {
+                                    fieldRef: {
+                                        fieldPath: "metadata.namespace",
+                                    },
+                                },
+                            },
+                            {
+                                name: "POD_NAME",
+                                valueFrom: {
+                                    fieldRef: {
+                                        fieldPath: "metadata.name",
+                                    },
+                                },
+                            },
+                            {
+                                name: "OPERATOR_NAME",
+                                value: "pulumi-kubernetes-operator",
+                            },
+                            {
+                                name: "GRACEFUL_SHUTDOWN_TIMEOUT_DURATION",
+                                value: "5m",
+                            },
+                            {
+                                name: "MAX_CONCURRENT_RECONCILES",
+                                value: "10",
+                            },
+
+
+                        ],
+                    }],
+                    // Should be same or larger than GRACEFUL_SHUTDOWN_TIMEOUT_DURATION
+                    terminationGracePeriodSeconds: 300,
+                },
+            },
+        },
+    }, deploymentOptions);
+}
