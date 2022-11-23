5f48353e64f9ca9ca70d0e0d2073da350dde6495
Michael Bridgen <mbridgen@pulumi.com>
2022-11-23 11:45:09 +0000

    Make trivial project for skip-deps test case
    We want to be able to check that using `.spec.skipInstallDependencies`
    will result in no (e.g.,) `npm install` being run; but, we also need to
    see the Stack succeed. So: here is a minimal project, which will succeed
    on its own; and, we'll add a spurious extra package, and observe that it
    doesn't also get installed.
    
    Signed-off-by: Michael Bridgen <mbridgen@pulumi.com>

---
+++ test/testdata/skip-deps/index.js
@@ -0,0 +1,15 @@
+"use strict";
+const k8s = require("@pulumi/kubernetes");
+
+const appLabels = { app: "nginx" };
+const deployment = new k8s.apps.v1.Deployment("nginx", {
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 1,
+        template: {
+            metadata: { labels: appLabels },
+            spec: { containers: [{ name: "nginx", image: "nginx" }] }
+        }
+    }
+});
+exports.name = deployment.metadata.name;
