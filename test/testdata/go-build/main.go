61328f0ebf32d7455be277f7173de46b2001cc51
Michael Bridgen <mbridgen@pulumi.com>
2023-02-02 15:17:59 +0000

    Test the controller works with the Go build cache
    A simple test to check that the controller doesn't cause the go build
    cache to grow, unduly. Essentially:
    
     1. Run the stack
     2. Don't change anything
     3. Run the stack again
     4. Check that the Go build cache didn't change between 1. and 3.
    
    If a stack is built in a fresh tmp directory each time (as it was
    previously), this test will fail, because at least the stack's main.go
    will look like a new file to the cache.
    
    Signed-off-by: Michael Bridgen <mbridgen@pulumi.com>

---
+++ test/testdata/go-build/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		ctx.Export("name", pulumi.String("foo"))
+		return nil
+	})
+}
