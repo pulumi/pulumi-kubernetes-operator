93340a9b0e31dbb934134170cf9561cc14741287
Ramon Quitales <ramon@pulumi.com>
2023-04-27 16:19:34 -0700

    chore: prepare manifests for v1.11.5 release
    Update image references for the upcoming v1.11.5 release.

--- deploy/deploy-operator-cs/MyStack.cs
+++ deploy/deploy-operator-cs/MyStack.cs
@@ -7,8 +7,8 @@ using Pulumi.Kubernetes.Types.Inputs.Rbac.V1;
 
 class MyStack : Stack
 {
-    public const string DefaultCRDVersion = "v1.11.0";
-    public const string DefaultOperatorVersion = "v1.11.0";
+    public const string DefaultCRDVersion = "v1.11.5";
+    public const string DefaultOperatorVersion = "v1.11.5";
 
     public MyStack()
     {
