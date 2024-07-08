c0a5defdcbcae92b7925b12203c9811d54032c3d
Bryce Lampe <bryce@pulumi.com>
2024-07-08 11:46:55 -0700

    Keep Helm CRDs in sync

--- scripts/generate_crds.sh
+++ scripts/generate_crds.sh
@@ -8,3 +8,5 @@ deploy_dir="$cwd/../deploy/crds"
 echo "Generating CRD API types..."
 
 controller-gen paths="$apis_dir/..." crd:crdVersions=v1 output:crd:dir="$deploy_dir"
+cp "${deploy_dir}/pulumi.com_programs.yaml" "${deploy_dir}/../helm/pulumi-operator/crds/program-crd.yaml"
+cp "${deploy_dir}/pulumi.com_stacks.yaml" "${deploy_dir}/../helm/pulumi-operator/crds/stack-crd.yaml"
