928cbabafec755b72c2cead9af6d94bcf60e8873
Eron Wright <eron@pulumi.com>
2024-10-02 15:34:18 -0700

    bugfix for fileserver

--- operator/cmd/main.go
+++ operator/cmd/main.go
@@ -86,7 +86,7 @@ func main() {
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
 	flag.StringVar(&programFSAddr, "program-fs-addr", envOrDefault("PROGRAM_FS_ADDR", ":9090"),
 		"The address the static file server binds to.")
-	flag.StringVar(&programFSAdvAddr, "program-fs-adv-addr", envOrDefault("PROGRAM_FS_ADV_ADDR", ""),
+	flag.StringVar(&programFSAdvAddr, "program-fs-adv-addr", envOrDefault("PROGRAM_FS_ADV_ADDR", "localhost:9090"),
 		"The advertised address of the static file server.")
 	opts := zap.Options{
 		Development: true,
