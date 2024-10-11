a4c8810ad2ab217c7148fe226fde6c8d5fb9a8f2
Bryce Lampe <bryce@pulumi.com>
2024-10-11 16:29:32 -0700

    [v2] Retry failed updates with exponential backoff (#709)
    Currently, if our automation APIs call fail they return non-nil errors
    to the operator. In #676 I modified `Update` to translate these errors
    into a "failed" status on the Update/Stack, but other operations
    (preview etc.) still surface these errors and automatically re-queue.
    
    We'd like to retry these failed updates much less aggressively than we
    retry transient network errors, for example. To accomplish this we do a
    few things:
    
    * We consolidate the update controller's streaming logic for consistent
    error handling across all operations.
    * We return errors with known gRPC status codes as-is, but unknown
    status codes are translated into failed results for all operations.
    * We start tracking the number of times a stack has attempted an update.
    This is used to determine how much exponential backoff to apply.
    * A failed update is considered synced for a cooldown period before we
    retry it. The cooldown period starts at 5 minutes and doubles for every
    failed attempt, eventually maxing out at 24 hours.
    
    Fixes #677

--- operator/api/pulumi/shared/stack_types.go
+++ operator/api/pulumi/shared/stack_types.go
@@ -388,6 +388,10 @@ type StackUpdateState struct {
 	Permalink Permalink `json:"permalink,omitempty"`
 	// LastResyncTime contains a timestamp for the last time a resync of the stack took place.
 	LastResyncTime metav1.Time `json:"lastResyncTime,omitempty"`
+	// Failures records how many times the update has been attempted and
+	// failed. Failed updates are periodically retried with exponential backoff
+	// in case the failure was due to transient conditions.
+	Failures int64 `json:"failures"`
 }
 
 // StackUpdateStatus is the status code for the result of a Stack Update run.
